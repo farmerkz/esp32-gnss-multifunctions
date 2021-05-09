5d04f553c92650a7f442358aad640814fbe067d8
farmerkz <andrey@zolotnitskiy.com>
2021-05-09 22:54:57 +0600

    Добавлена минимальная система логирования

--- src/fatalerror.cpp
+++ src/fatalerror.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 
 extern void changeColor(uint _red, uint _green, uint _blue);
+extern File logFile;
 
 /** @brief Фатальная ошибка, мигаем красным светодиодом LED_RED_FATAL_BLINK раз и уходим
  * в Deep Sleep на DEEP_SLEEP_TIME
@@ -30,13 +31,18 @@ void fatalError()
   ESP.deepSleep(DEEP_SLEEP_TIME);
 } // fatalError()
 
-/** @brief Фатальная ошибка, мигаем красным светодиодом N (параметр) раз и уходим
+/** @brief Ошибка. Мигаем красным светодиодом N раз, пишем
+ * в файл error.log сообщение и, если установлен флаг, уходим
  * в Deep Sleep на DEEP_SLEEP_TIME
- * @param n количество циклов моргания сетодиодом и писков
+ * @param n количество циклов моргания светодиодом и писков
+ * @param event строка для записи в файл error.log
+ * @param deepsl флаг ухода в deep sleep, по умолчанию true
  */
-void fatalError(uint8_t _n)
+void fatalError(uint8_t _n, const char *_event, bool _deepsl = true)
 {
   disableLoopWDT();
+  logFile.println(_event);
+  logFile.flush();
   for (int i = 0; i < _n; i++)
   {
     ledcWriteTone(BUZZER_CHAN, 700);
@@ -53,8 +59,11 @@ void fatalError(uint8_t _n)
     }
   }
   ledcWriteTone(BUZZER_CHAN, 0);
-  esp_bluedroid_disable();
-  esp_bt_controller_disable();
-  esp_wifi_stop();
-  ESP.deepSleep(DEEP_SLEEP_TIME);
+  if (_deepsl)
+  {
+    esp_bluedroid_disable();
+    esp_bt_controller_disable();
+    esp_wifi_stop();
+    ESP.deepSleep(DEEP_SLEEP_TIME);
+  }
 }
